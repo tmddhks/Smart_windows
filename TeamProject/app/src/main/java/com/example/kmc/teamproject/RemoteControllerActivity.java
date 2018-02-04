7122f1c4427a677d9c282ae20754e12757a95a37
kmc1231 <mckim1231@hanmail.net>
2018-02-04 16:29:38 +0900

    기본디자인

---
+++ TeamProject/app/src/main/java/com/example/kmc/teamproject/RemoteControllerActivity.java
@@ -0,0 +1,13 @@
+package com.example.kmc.teamproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RemoteControllerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote_controller);
+    }
+}
