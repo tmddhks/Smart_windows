c10bc32afa28e5e0686cc1d107bf6e7ca42d42ae
kmc1231 <mckim1231@hanmail.net>
2018-02-12 18:03:44 +0900

    비상전화 추가 imagebutton(action_dial)

--- TeamProject/app/src/main/java/com/example/kmc/teamproject/RemoteControllerActivity.java
+++ TeamProject/app/src/main/java/com/example/kmc/teamproject/RemoteControllerActivity.java
@@ -1,7 +1,12 @@
 package com.example.kmc.teamproject;
 
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class RemoteControllerActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class RemoteControllerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote_controller);
+
+        ImageButton HelpCall=(ImageButton)findViewById(R.id.ibHelpCall);
+        HelpCall.setOnClickListener(new ImageButton.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:01012345678"));
+                startActivity(intent);
+            }
+        });
     }
+
 }
