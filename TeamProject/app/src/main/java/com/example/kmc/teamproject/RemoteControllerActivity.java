68fd7a0b99eea3cda4e07ce3ef2d83a35845d633
kmc1231 <mckim1231@hanmail.net>
2018-02-12 19:53:52 +0900

    위젯 기본 틀

--- TeamProject/app/src/main/java/com/example/kmc/teamproject/RemoteControllerActivity.java
+++ TeamProject/app/src/main/java/com/example/kmc/teamproject/RemoteControllerActivity.java
@@ -5,6 +5,7 @@ import android.media.Image;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -15,8 +16,8 @@ public class RemoteControllerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote_controller);
 
-        ImageButton HelpCall=(ImageButton)findViewById(R.id.ibHelpCall);
-        HelpCall.setOnClickListener(new ImageButton.OnClickListener(){
+        ImageButton HelpCall = (ImageButton) findViewById(R.id.ibHelpCall);
+        HelpCall.setOnClickListener(new ImageButton.OnClickListener() {
 
             @Override
             public void onClick(View view) {
